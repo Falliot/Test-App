d15766868dc5baf54b5541f445a9dd57037ba6df
Anton <pustovidko.anton@icloud.com>
2020-02-10 18:07:32 +0100

    init commit

---
+++ Test app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test app
+//
+//  Created by Anton on 2/10/20.
+//  Copyright © 2020 falli_ot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
