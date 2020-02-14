a769c60ba945e57fbac997d20a47408cc0df1ef0
Anton <pustovidko.anton@icloud.com>
2020-02-14 13:06:57 +0200

    open camera and write image dimensions to the screen

--- Test app/ViewController.swift
+++ Test app/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    
+    
+    @IBOutlet weak var dimensionsLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func cameraButton(_ sender: Any) {
+        
+        let camera = UIImagePickerController()
+        camera.sourceType = .camera
+        camera.allowsEditing = false
+        camera.delegate = self
+        present(camera, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        guard let image = info[.originalImage] as? UIImage else{
+            print("Image not found")
+            return
+        }
+        //Printing image dimensions in the terminal
+        print(image.size)
+        
+        dimensionsLabel.text = "Size: \(String(format:"%.0f" ,image.size.width)) x  \(String(format: "%0.f", image.size.height))"
+        
+    }
+    
+    
+    
+    @IBAction func sendButton(_ sender: Any) {
+        
+    }
+    
+    
+    
 
 }
 
