5e2ef8b963f00d6e6e0efa5a40be7629894f4b30
Anton <pustovidko.anton@icloud.com>
2020-02-16 22:41:22 +0200

    1. image replace 2. add comments 3.make UNIT TESTS

---
+++ Test appTests/Test_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_appTests.swift
+//  Test appTests
+//
+//  Created by Anton on 2/16/20.
+//  Copyright © 2020 falli_ot. All rights reserved.
+//
+
+import XCTest
+
+class Test_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
